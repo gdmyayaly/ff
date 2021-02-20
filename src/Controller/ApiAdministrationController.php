06a14311017cd92a31378b6ed3f4472e8027be9e
gdmyayaly <devyayaly@gmail.com>
2021-02-20 11:24:07 +0100

    ddd

---
+++ src/Controller/ApiAdministrationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+    /**
+     * @Route("/yaya", name="api_administration")
+     */
+class ApiAdministrationController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function getUserInscris(){
+        
+    }
+}
